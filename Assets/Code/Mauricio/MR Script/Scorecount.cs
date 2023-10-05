889f13b558cfb982ea56de86f9b2b23eb683753e
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-05 01:14:16 -0700

    Added a function

--- Assets/Code/Mauricio/MR Script/Scorecount.cs
+++ Assets/Code/Mauricio/MR Script/Scorecount.cs
@@ -10,10 +10,12 @@ public class Scorecount : MonoBehaviour {
     public void OnCollisionEnter2D(Collision2D collision){
         Debug.Log("Made it into the second function.");
         if (collision.gameObject.CompareTag("Player")){
-            Score++;
-            Debug.Log(Score);
-            ScoreBoard.text = "Score: "+ Score;
+            Addition();
         }
 
+    }
+    void Addition(){
+        Score++;
+        ScoreBoard.text = "Score: "+ Score;
     }
 }
