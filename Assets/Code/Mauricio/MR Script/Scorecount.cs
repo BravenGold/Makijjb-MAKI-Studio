2ee2d31656348ed0634a586e63bfdb745db69f7a
BravenGold <102940693+BravenGold@users.noreply.github.com>
2023-10-05 01:09:38 -0700

    Update Scorecount.cs

--- Assets/Code/Mauricio/MR Script/Scorecount.cs
+++ Assets/Code/Mauricio/MR Script/Scorecount.cs
@@ -3,13 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
-public class Scorecount : MonoBehaviour{
+public class Scorecount : MonoBehaviour {
     public TextMeshProUGUI ScoreBoard;
     public float Score = 0f;
 
     public void OnCollisionEnter2D(Collision2D collision){
+        Debug.Log("Made it into the second function.");
         if (collision.gameObject.CompareTag("Player")){
             Score++;
+            Debug.Log(Score);
             ScoreBoard.text = "Score: "+ Score;
         }
 
