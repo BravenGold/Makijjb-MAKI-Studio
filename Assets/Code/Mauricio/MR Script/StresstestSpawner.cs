de4cd6e35a282670edfe13b65faafc0075cbb858
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-19 08:05:34 -0700

    Stress test fixed

---
+++ Assets/Code/Mauricio/MR Script/StresstestSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFoodSpawner : MonoBehaviour{
+
+    [SerializeField] GameObject[] FoodPrefab;
+    [SerializeField] float SpawnTime = 0.001f;
+    [SerializeField] float MinTrans;
+    [SerializeField] float MaxTrans;
+    private float amount = 9999999999999990999;
+    float a = 0; 
+
+    // Start is called before the first frame update
+    void Start(){
+        StartCoroutine(FoodSpawn());
+    }
+
+    // Update is called once per frame
+    IEnumerator FoodSpawn(){
+        while(a<(amount+amount)){
+            var wanted = Random.Range(MinTrans, MaxTrans);
+            //var position = new Vector3(wanted,transform.position.y);
+            var position = transform.position - new Vector3(wanted,1,0);
+            GameObject gameObject = Instantiate(FoodPrefab[Random.Range(0,FoodPrefab.Length)], position, Quaternion.identity);
+            //remove this to breakgame
+            //yield return new WaitForSeconds(SpawnTime);
+            //Destroy(gameObject, 5f);
+            yield return 0;
+            a++;
+        }
+    }
+}
