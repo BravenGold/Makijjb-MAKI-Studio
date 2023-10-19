3bcb0b0e53300c72ed18f95fbb2a16f5d329384a
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-10-19 00:16:55 -0700

    StressTest

--- Assets/Code/Mauricio/MR Script/FoodSpawner.cs
+++ Assets/Code/Mauricio/MR Script/FoodSpawner.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class FoodSpawner : MonoBehaviour{
 
     [SerializeField] GameObject[] FoodPrefab;
-    [SerializeField] float SpawnTime = 0.5f;
+    [SerializeField] float SpawnTime = 5f;
     [SerializeField] float MinTrans;
     [SerializeField] float MaxTrans;
+    private int itemsSpawned = 0; // Counter for spawned items
+    private int maxItemsToSpawn = 3; // Maximum items to spawn
 
     // Start is called before the first frame update
     void Start(){
@@ -16,16 +18,15 @@ public class FoodSpawner : MonoBehaviour{
 
     // Update is called once per frame
     IEnumerator FoodSpawn(){
-        while(true){
+        while(itemsSpawned < maxItemsToSpawn){
             var wanted = Random.Range(MinTrans, MaxTrans);
-            var position = new Vector3(wanted,transform.position.y);
+            //var position = new Vector3(wanted,transform.position.y);
+            var position = transform.position - new Vector3(wanted,1,0);
             GameObject gameObject = Instantiate(FoodPrefab[Random.Range(0,FoodPrefab.Length)], position, Quaternion.identity);
+            //remove this to breakgame
+            itemsSpawned++; // Increment the counter for spawned items
             yield return new WaitForSeconds(SpawnTime);
             //Destroy(gameObject, 5f);
         }
-    }
-    void Update()
-    {
-        
     }
 }
