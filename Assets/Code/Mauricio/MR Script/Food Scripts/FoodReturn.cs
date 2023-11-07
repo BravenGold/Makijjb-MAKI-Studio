8610b6746f9d16a60cd5c71b1edfeac06d989c43
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-11-06 21:41:21 -0800

    Level1SetUp

--- Assets/Code/Mauricio/MR Script/Food Scripts/FoodReturn.cs
+++ Assets/Code/Mauricio/MR Script/Food Scripts/FoodReturn.cs
@@ -10,33 +10,79 @@ public class FoodReturn : MonoBehaviour{
     public KeyCode interactKey;
     //public UnityEvent interactAction;
     private FoodObjectPool objectPool;
+    private SteakPool Spool;
+    private SaladPool Sapool;
     private Collider2D foodCollision;
+    private int foodid = 0;
     // Start is called before the first frame update
     void Start(){
         objectPool = FindAnyObjectByType<FoodObjectPool>();
+        Spool = FindAnyObjectByType<SteakPool>();
+        Sapool = FindAnyObjectByType<SaladPool>();
     }
 
     // Update is called once per frame
     void Update(){
-        if(isInRange){
+        if(isInRange && foodid==1){
             if(Input.GetKeyDown(interactKey)){
-                Debug.Log("test");
                 objectPool.ReturnFood(foodCollision.gameObject);
             }
+        }else if(isInRange&&foodid==2){
+            if(Input.GetKeyDown(interactKey)){
+                Spool.ReturnSteak(foodCollision.gameObject);
+            }
+        }else if(isInRange&&foodid==3){
+            if(Input.GetKeyDown(interactKey)){
+                Sapool.ReturnSalad(foodCollision.gameObject);
+            }
+        }else if(isInRange&&foodid==4){
+            Debug.Log("Chicken Holder");
         }
     }
     private void OnTriggerEnter2D(Collider2D collision){
         if(collision.gameObject.CompareTag("Food")){
             isInRange = true;
             foodCollision = collision;
+            foodid = 1;
             Debug.Log("Food is now in range");
+        }else if(collision.gameObject.CompareTag("Steak")){
+            isInRange = true;
+            foodCollision = collision;
+            foodid=2;
+            Debug.Log("Steak is now in range");
+        }else if(collision.gameObject.CompareTag("Salad")){
+            isInRange = true;
+            foodid=3;
+            foodCollision = collision;
+            Debug.Log("Salad is now in range");
+        }else if(collision.gameObject.CompareTag("Chicken")){
+            isInRange = true;
+            foodCollision = collision;
+            foodid=4;
+            Debug.Log("Chicken is now in range");
         }
     }
     private void OnTriggerExit2D(Collider2D collision){
         if(collision.gameObject.CompareTag("Food")){
             isInRange = false;
+            foodid=0;
             Debug.Log("Food is now out of range");
-        }        
+        }else if(collision.gameObject.CompareTag("Steak")){
+            isInRange = false;
+            foodCollision = collision;
+            foodid=0;
+            Debug.Log("Steak is now in range");
+        }else if(collision.gameObject.CompareTag("Salad")){
+            isInRange = false;
+            foodid=0;
+            foodCollision = collision;
+            Debug.Log("Salad is now out of range");
+        }else if(collision.gameObject.CompareTag("Chicken")){
+            isInRange = false;
+            foodCollision = collision;
+            foodid=0;
+            Debug.Log("Chicken is out of range");
+        }
     }
 }
 
--- Assets/Test/Mauricio/PlayModeTests/spawn.cs
+++ Assets/Test/Mauricio/PlayModeTests/spawn.cs
@@ -6,12 +6,40 @@ using UnityEngine.TestTools;
 
 public class spawn{
 
+    private GameObject gameObject;
+    private SteakPool Spawntest;
+
+    private GameObject SaladObject;
+    private SaladPool Saladtest;
+
+    [SetUp]
+    public void Setup()
+    {
+        // Create a new GameObject and add the SteakPool component to it.
+        gameObject = new GameObject();
+        Spawntest = gameObject.AddComponent<SteakPool>();
+        Spawntest.Steakprefab = Resources.Load<GameObject>("Prefab/rawmeat");
+
+        //This is the set up for the saladpool and a game object
+        SaladObject = new GameObject();
+        Saladtest = SaladObject.AddComponent<SaladPool>();
+        Saladtest.Saladprefab = Resources.Load<GameObject>("Prefab/salad");
+    }
     [UnityTest]
-    public IEnumerator spawnWithEnumeratorPasses(){
+    public IEnumerator LimiterWithEnumeratorPasses(){
+        //This checks to see that only 10 steak items are created. 
+        int spawnum = 0;
+        for(spawnum=0;spawnum<50;spawnum++){
+            Spawntest.GetSteak();
+        }
+        Assert.AreEqual(10,Spawntest.Foodcount());
+        //This checks that only 10 salad food items are made. 
+        spawnum = 0;
+        for(spawnum=0;spawnum<50;spawnum++){
+            Saladtest.GetSalad();
+        }
+        Assert.AreEqual(10,Saladtest.Foodcount());
 
-        var gameObject = new GameObject();
-        var T = gameObject.AddComponent<FoodSpawner>();
-        yield return null;
-
+        return null;
     }
 }
