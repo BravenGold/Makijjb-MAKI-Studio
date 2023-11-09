9d6e818db3a0ba0c0b2032a43403acead2a676d6
mauricio5450 <mauricio.rodriguez5450@gmail.com>
2023-11-09 02:23:56 -0800

    PointSystem

---
+++ Assets/Code/Mauricio/MR Script/GameManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text ScoreText;
+    public int Score = 1;
+    public int maxScore;
+
+    public GameObject Youtext;
+
+    void start(){
+        Score = 0;
+    }
+    public void AddScore(int newScore){
+        Score+=newScore;
+    }
+
+    public void UpdateScore(){
+        ScoreText.text = "Score "+Score;
+    }
+    void Update(){
+        UpdateScore();
+        if(Score==maxScore){
+            Youtext.SetActive(true);
+        }
+    }
+}
