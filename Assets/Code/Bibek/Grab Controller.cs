98b9e8ac382313cc626826879139c89d83a6d102
Bibek <46560351+Ichigo786@users.noreply.github.com>
2023-12-07 01:15:13 -0800

    fixed the grab function

--- Assets/Code/Bibek/Grab Controller.cs
+++ Assets/Code/Bibek/Grab Controller.cs
@@ -4,36 +4,53 @@ using UnityEngine;
 
 public class GrabController : MonoBehaviour
 {
-
     public Transform grabDetect;
     public Transform boxHolder;
     public float rayDist;
 
+    private bool isHoldingItem = false;
+    private GameObject heldItem = null;
+
     void Update()
     {
-
         RaycastHit2D grabCheck = Physics2D.Raycast(grabDetect.position, Vector2.right * transform.localScale, rayDist);
-        // Debug.Log("Update function.");
 
-        if (grabCheck.collider != null && (grabCheck.collider.tag == "Steak" || grabCheck.collider.tag == "Salad"))
+        if (grabCheck.collider != null && (grabCheck.collider.tag == "Steak" || grabCheck.collider.tag == "Salad" || grabCheck.collider.tag == "Chicken"))
         {
-            // Debug.Log("Grab checked.");
-            if (Input.GetKey(KeyCode.G))
+            if (Input.GetKeyDown(KeyCode.G))
             {
-                // Debug.Log("Food tried to be grabbed.");
-                grabCheck.collider.gameObject.transform.parent = boxHolder;
-                grabCheck.collider.gameObject.transform.position = boxHolder.position;
-                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = true;
-
+                if (!isHoldingItem)
+                {
+                    // Pick up the item
+                    heldItem = grabCheck.collider.gameObject;
+                    heldItem.transform.parent = boxHolder;
+                    heldItem.transform.position = boxHolder.position;
+                    heldItem.GetComponent<Rigidbody2D>().isKinematic = true;
+                    isHoldingItem = true;
+                }
+                else
+                {
+                    // Release the item
+                    if (heldItem != null)
+                    {
+                        heldItem.transform.parent = null;
+                        heldItem.GetComponent<Rigidbody2D>().isKinematic = false;
+                        heldItem = null;
+                    }
+                    isHoldingItem = false;
+                }
             }
-            else
-            {
-                grabCheck.collider.gameObject.transform.parent = null;
-                grabCheck.collider.gameObject.GetComponent<Rigidbody2D>().isKinematic = false;
-
-            }
-
         }
-
+        else if (isHoldingItem && Input.GetKeyDown(KeyCode.G))
+        {
+            // Release the item if the player is holding one but not over a grabbable object
+            if (heldItem != null)
+            {
+                heldItem.transform.parent = null;
+                heldItem.GetComponent<Rigidbody2D>().isKinematic = false;
+                heldItem = null;
+            }
+            isHoldingItem = false;
+        }
     }
 }
